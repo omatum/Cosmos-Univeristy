deff2fad2edaaa22a64ad2b950167cd8b4b67efa
Greg Dizzia <gregdizzia@gmail.com>
2022-11-07 00:47:41 -0500

    WHOA. Increased build performance. From 70 seconds to 1.938 seconds by globalizing data variables. Also: basic plant page is set up

---
+++ _plugins/lets_get_data.rb
@@ -0,0 +1,7 @@
+
+$db_celestials = JSON.parse(File.read(File.expand_path("../../_data/celestials.json",__FILE__)))
+$db_chakras = JSON.parse(File.read(File.expand_path("../../_data/chakras.json",__FILE__)))  
+$db_crystals = JSON.parse(File.read(File.expand_path("../../_data/crystals.json",__FILE__))) 
+$db_elements = JSON.parse(File.read(File.expand_path("../../_data/elements.json",__FILE__)))  
+$db_magic_effects = JSON.parse(File.read(File.expand_path("../../_data/magic_effects.json",__FILE__))) 
+$db_plant_magicals = JSON.parse(File.read(File.expand_path("../../_data/plant_magicals.json",__FILE__))) 
